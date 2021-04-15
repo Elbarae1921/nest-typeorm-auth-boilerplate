f5daf5c59d908805fa6888b4f7fea0a32b868573
Elbarae <elbarae1921@gmail.com>
2021-04-15 13:43:16 +0000

    🥷 Forgot to remove my top secret debbuging technique

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -15,7 +15,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(payload: AuthPayload) {
-        console.log('SHIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIT');
         const { id } = payload;
         const user = await this.userService.findById(id);
         if (!user) {
