5602a06b8ad6865cf5b2d22d1b253e2926ccd948
Elbarae <elbarae1921@gmail.com>
2021-04-27 16:44:56 +0000

    Added the port as an env variable

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     app.setGlobalPrefix("api");
-    app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
-    await app.listen(3000);
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+    await app.listen(process.env.PORT);
 }
 bootstrap();
